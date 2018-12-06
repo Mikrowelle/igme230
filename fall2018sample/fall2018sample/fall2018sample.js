9a7c11601ac19a7eda3b8af4a847041337eb4762
Andrew Lefurge <andlefurge@gmail.com>
2018-12-06 14:28:13 -0500

    finished final layout

---
+++ fall2018sample/fall2018sample/fall2018sample.js
@@ -0,0 +1,12 @@
+/* Don't use <script> tags in a linked js file! */
+$(".menuitem").click(function () {
+	$(this).next(".submenu").slideToggle("fast");
+});
+ let verse = ("content1.txt") // sets default verse element
+    $("#choose-content").val(verse); // changes menu option to default
+    $("#content").load(verse); // retrieves only default element
+
+    $("#choose-content").change(function() {
+      verse = $(this).val();
+      $("#content").load(verse);
+    });
