8102c70867a28aa646a633a37bac94ff14ebcd5a
Andrew Lefurge (RIT Student) <acl3700@boo-1303-01.main.ad.rit.edu>
2018-12-08 21:52:55 -0500

    got the arrow to flip when you click projects

--- final/interaction.js
+++ final/interaction.js
@@ -1,12 +1,6 @@
 	$(".hideshow").click(function () {
 	    $(this).next(".projgallery").slideToggle("slow");
-	});
+        /*OMG IT TOOK ME SO LONG TO FIGURE OUT HOW TO TARGET THIS*/
+        $(this).children(".hideshowicon").children("i").toggle();
 
-	function myFunction() {
-	    var x = document.getElementById("myTopnav");
-	    if (x.className === "links") {
-	        x.className += " responsive";
-	    } else {
-	        x.className = "links";
-	    }
-	}
+	});
