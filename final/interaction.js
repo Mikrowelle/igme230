fdf984707253029ecf6d147afd2baced4e0abcd7
Andrew Lefurge (RIT Student) <acl3700@boo-1303-01.main.ad.rit.edu>
2018-12-08 20:44:13 -0500

    slight mobile improvements and bad hamburger menu

--- final/interaction.js
+++ final/interaction.js
@@ -1,3 +1,12 @@
-	$(".hideshow").click(function() {
-		$(this).next(".projgallery").slideToggle("slow");
+	$(".hideshow").click(function () {
+	    $(this).next(".projgallery").slideToggle("slow");
 	});
+
+	function myFunction() {
+	    var x = document.getElementById("myTopnav");
+	    if (x.className === "links") {
+	        x.className += " responsive";
+	    } else {
+	        x.className = "links";
+	    }
+	}
