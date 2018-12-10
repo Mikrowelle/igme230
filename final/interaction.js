ef883e4d34664e232a4b47272adbc5f4bb45bc1e
Andrew Lefurge (RIT Student) <acl3700@boo-1303-20.main.ad.rit.edu>
2018-12-09 19:30:12 -0500

    fixed a bunch of stuff with the nav

--- final/interaction.js
+++ final/interaction.js
@@ -1,94 +1,111 @@
+"use strict";
+
 /*Flips the exband and hide arrows*/
 $(".hideshow").click(function () {
-	    $(this).next(".projgallery").slideToggle("slow");
-	    /*OMG IT TOOK ME SO LONG TO FIGURE OUT HOW TO TARGET THIS*/
-	    $(this).children(".hideshowicon").children("i").toggle();
+    $(this).next(".projgallery").slideToggle("slow");
+    /*OMG IT TOOK ME SO LONG TO FIGURE OUT HOW TO TARGET THIS*/
+    $(this).children(".hideshowicon").children("i").toggle();
 
-	});
+});
 
-/*Navigation Scroll Behaviors*/
-	$(window).scroll(function () {
-	    var $height = $(window).scrollTop();
-	    if ($height > 50) {
-	        $("nav").css({
-	            "background-color": "black",
-	            "opacity": ".8"
-	        });
-	        $("a").css({
-	            "color": "#fafafa",
-	            "opacity": "1"
-	        });
-	        $(".site_name").css({
-	            "color": "#fafafa",
-	            "opacity": "1"
-	        });
-	        $("header div").css({
-	            "opacity": "0"
-	        });
-	        $("#return_button").css({
-	            "opacity": ".8"
-	        });
-	    } else {
-	        $("nav").css({
-	            "opacity": "1",
-	            "background": "none"
-	        });
-	        $("a").css({
-	            "color": "#333333"
-	        });
-	        $(".site_name").css({
-	            "color": "#333333"
-	        });
-	        $("header h1").css({
-	            "opacity": ".8"
-	        });
-	        $("header div").css({
-	            "opacity": ".8"
-	        });
-	        $("#return_button").css({
-	            "opacity": "0"
-	        });
-	    }
-	});
+/*Navigation Changes on Scroll*/
+$(window).scroll(function () {
+    var $height = $(window).scrollTop();
+    if ($height > 50) {
+        $("nav").css({
+            "background-color": "black",
+            "opacity": ".8"
+        });
+        $(".dropdown-content").css({
+            "background-color": "black",
+            "opacity": "1"
+        });
+        $(".dropbtn").css({
+            "color": "#fafafa"
+        });
+
+        $("a").css({
+            "color": "#fafafa",
+            "opacity": "1"
+        });
+        $(".site_name").css({
+            "color": "#fafafa",
+            "opacity": "1"
+        });
+        $("header div").css({
+            "opacity": "0"
+        });
+        $("#return_button").css({
+            "opacity": ".8"
+        });
+    } else {
+        $("nav").css({
+            "opacity": "1",
+            "background": "none"
+        });
+        $(".dropdown-content").css({
+            "background-color": "#fafafa",
+            "opacity": ".8"
+        });
+        $(".dropbtn").css({
+            "color": "#333333"
+        });
+        $("a").css({
+            "color": "#333333"
+        });
+        $(".site_name").css({
+            "color": "#333333"
+        });
+        $("header h1").css({
+            "opacity": ".8"
+        });
+        $("header div").css({
+            "opacity": ".8"
+        });
+        $("#return_button").css({
+            "opacity": "0"
+        });
+    }
+});
 
 /*Smooth Scroll*/
-	$('header a').click(function () {
-	    let target = $(this.hash);
-	    $('html, body').animate({
-	        scrollTop: target.offset().top - 65
-	    }, 600);
-	});
-	$('#return_button a').click(function () {
-	    let target = $(this.hash);
-	    $('html, body').animate({
-	        scrollTop: target.offset().top
-	    }, 600);
-	});
+$('header a').click(function () {
+    let target = $(this.hash);
+    $('html, body').animate({
+        scrollTop: target.offset().top - 65
+    }, 600);
+});
+$('#return_button a').click(function () {
+    let target = $(this.hash);
+    $('html, body').animate({
+        scrollTop: target.offset().top
+    }, 600);
+});
 
 /*Side Nav Panel*/
-	$("#close").click(function () {
-	    $("#navpanel").animate({
-	        left: '-250'
-	    }, "slow");
-	    $("#close").toggle();
-	    $("#open").toggle();
-	    $("#navpanel").css({
-	        "opacity": ".5"
-	    });
-	});
+$("#close").click(function () {
+    $("#navpanel").animate({
+        left: '-250'
+    }, "slow");
+    $("#close").toggle();
+    $("#open").toggle();
+    $("#navpanel").css({
+        "opacity": ".5"
+    });
+});
 
-	$("#open").click(function () {
-	    $("#navpanel").animate({
-	        left: '0px'
-	    }, "slow");
-	    $("#close").toggle();
-	    $("#open").toggle();
-	    $("#navpanel").css({
-	        "opacity": ".8"
-	    });
-	});
+$("#open").click(function () {
+    $("#navpanel").animate({
+        left: '0px'
+    }, "slow");
+    $("#close").toggle();
+    $("#open").toggle();
+    $("#navpanel").css({
+        "opacity": ".8"
+    });
+});
 
 
-	$(".submenu").click(function () {
-	    $(this).next(".submenu-panel").toggle();
-	});
+$(".submenu").click(function () {
+    $(this).next(".submenu-panel").toggle();
+});
