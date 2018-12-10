fc26ca54c021080ee9b38f13be34c0c23a03792e
Andrew Lefurge (RIT Student) <acl3700@boo-1303-20.main.ad.rit.edu>
2018-12-10 00:30:10 -0500

    Undeleted important stuff

--- final/interaction.js
+++ final/interaction.js
@@ -1,5 +1,22 @@
 "use strict";
 
+//makes the carousal change
+    var myIndex = 0;
+    carousel();
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("slide");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 4000);
+    }
+
 /*Flips the exband and hide arrows*/
 $(".hideshow").click(function () {
     $(this).next(".projgallery").slideToggle("slow");
